1865c3721703d4115273acf0469da89ea39ccf11
Dwi Septa Satria Agung <septasatria20@gmail.com>
2025-12-31 16:15:39 +0700

    feat: update public disk root path to use storage_path for consistency

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('storage'), // Langsung ke public/storage untuk shared hosting
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
